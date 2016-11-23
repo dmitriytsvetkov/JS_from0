038f73497c3ca27179b459bd437dff82d1313ac9
dmitriytsvetkov <dstvetkov@gmail.com>
2016-11-23 23:33:56 +0300

    закончил тема рекурсия и стеки

--- JS/recursion.js
+++ JS/recursion.js
@@ -1,4 +1,19 @@
+// Числа Фибоначчи
+/*function fib(n) {
+	return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+alert(fib(3));
+*/
+/*// Факториал числа n
+function factorial(n) {
+	if (n != 0) {
+		return n * factorial(n-1);
+	} else return 1;
+}
+alert(factorial(5));*/
+
 // Сумма чисел до n числа
+/* функция
 function sumTo(n) {
 	if (n != 0) {
 		return n + sumTo(n-1);
@@ -6,7 +21,7 @@ function sumTo(n) {
 	
 }
 
-alert(sumTo(100));
+alert(sumTo(100));*/
 
 
 
